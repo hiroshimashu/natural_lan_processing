cdcddc4b4c0467bb817d82171852c1fbdff251b2
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-09-18 16:02:30 +0900

    implement simple word similarity comparison

--- common/util.py
+++ common/util.py
@@ -39,6 +39,34 @@ def cos_similarity(x, y, eps = 1e-8):
     ny = y / np.sqrt(np.sum(y ** 2) + eps)
     return np.dot(nx, ny)
 
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    #search query
+    if query not in word_to_id:
+        print("%s is not found" % query)
+        return
+    
+    print('\n[query] ' + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    #calculate cos-similarity
+    vocab_size = len(word_to_id)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+    
+    #return related word
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(" %s: %s" % (id_to_word[i], similarity[i]))
+
+        count += 1
+        if count >= top:
+            return 
+
+
 
 
 
